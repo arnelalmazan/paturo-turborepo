b1cfc670971282ec2ebe1002af6736e813eeb16d
arnel360pixels <arnel@360pixels.co>
2025-04-22 22:40:02 +0800

    additional setup with pre-commit validation

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,22 +1,26 @@
-import Image, { type ImageProps } from "next/image"
-import { Button } from "@repo/ui/button"
-import { User } from "@repo/types"
-import styles from "./page.module.css"
-import { getUsers } from "../server/users"
+// import { User } from '@repo/types'
+import styles from './page.module.css'
+// import { getUsers } from '../server/users'
 
 export default async function Home() {
-  const users: User[] = await getUsers()
+  // const users: User[] = await getUsers()
+
+  // return (
+  //   <div className={styles.page}>
+  //     <main className={styles.main}>
+  //       {users.map((user) => (
+  //         <div key={user.id}>
+  //           <h1>{user.name}</h1>
+  //           <p>{user.email}</p>
+  //         </div>
+  //       ))}
+  //     </main>
+  //   </div>
+  // )
 
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        {users.map((user) => (
-          <div key={user.id}>
-            <h1>{user.name}</h1>
-            <p>{user.email}</p>
-          </div>
-        ))}
-      </main>
+      <main className={styles.main}>HOME PAGE</main>
     </div>
   )
 }
